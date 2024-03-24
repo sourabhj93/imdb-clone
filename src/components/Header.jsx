9ab35ce91fac9d4bbfbe09d69e7b4c4454aea038
Sourabh Jain <sourabh.jain@nagarro.com>
2024-03-24 17:25:56 +0530

    added header for desktop and mobile view

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import {AiFillHome} from 'react-icons/ai'
+import {BsCircleFill} from 'react-icons/bs'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+        <div className='flex gap-4'>
+            <MenuItem title="Home" address="/" Icon={AiFillHome}/>
+            <MenuItem title="About" address="/about" Icon={BsCircleFill}/>
+        </div>
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <div className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-xl'>imDB</div>
+          <div className='text-xl hidden sm:inline'>clone</div>
+        </Link>
+    </div>
+  )
+}
