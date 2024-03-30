fe4b5c8566d750081d944a18330fb19a8aa4730a
Sourabh Jain <sourabh.jain@nagarro.com>
2024-03-30 12:24:12 +0530

    added about route

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 p-3'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Hi</p>
+      <p>Hello</p>
+      <p>How are you?</p>
+    </div>
+  )
+}
